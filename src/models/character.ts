73982ac1ca5613e1110ebd433c382e9da59213c1
Evelyn Olmedo <evelynolmeedo@gmail.com>
2024-11-07 18:18:49 -0300

    Add models to create a character

---
+++ src/models/character.ts
@@ -0,0 +1,58 @@
+class Character {
+    private _name: string;
+    private _level: number;
+    private _health: number;
+    private _experience: number;
+    private _inventory: string[];
+
+    constructor(name: string, level: number, health: number, experience: number, inventory: string[]) {
+        this._name = name
+        this._level = level
+        this._health = health
+        this._experience = experience
+        this._inventory = inventory
+    }
+
+    // Getter para obtener el nombre del personaje
+    public get name(): string {
+        return this._name
+    }
+
+    public get level(): number {
+        return this._level
+    }
+
+    public get health(): number {
+        return this._health
+    }
+
+    public get experience(): number {
+        return this._experience
+    }
+
+    public get inventory(): string[] {
+        return this._inventory;
+    }
+
+    // Setter modifica el valor de una propiedad privada, permite controlar como se asignan los valores
+    public set name(value: string){
+        this._name;
+    }
+
+    public set level(value: number){
+        this._level;
+    }
+
+    public set health(value: number){
+        this._health
+    }
+
+    public set experience(value: number){
+        this._experience
+    }
+
+    public set inventory(value: string[]){
+        this._inventory
+    }
+}
+
