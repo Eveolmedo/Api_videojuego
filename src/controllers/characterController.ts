b3d7169ea8059d780af81a69ec1d5629f2137bce
Evelyn Olmedo <evelynolmeedo@gmail.com>
2024-11-18 18:41:21 -0300

    Add Handle errors in character

--- src/controllers/characterController.ts
+++ src/controllers/characterController.ts
@@ -5,33 +5,48 @@ import { Mage } from "../models/Mage";
 export let characters: Character[] = []
 
 export function createCharacter(name: string, level: number, health: number, type: "Warrior" | "Mage" = "Warrior"){ //Valor default warrior
-    let character: Character;
-  
-    if (type === "Warrior") {
-        character = new Warrior(name, level, health);
-    } else {
-        character = new Mage(name, level, health);
+    try {
+        let character: Character;
+    
+        if (type === "Warrior") {
+            character = new Warrior(name, level, health);
+        } else {
+            character = new Mage(name, level, health);
+        }
+    
+        characters.push(character);
+        return character;
+    } catch (error) {
+        return console.error("Error al crear un personaje", error)
     }
-  
-    characters.push(character);
-    return character;
 }
 
 export function listCharacters(){
-    return characters;
+    try {
+        characters.length === 0 ? "No hay personajes disponibles" : characters;
+    } catch (error) {
+        return console.error("Error al listar personajes", error);
+    }
 }
 
-export function updateCharacter(name: string, level?: number, health?: number, experience?: number): boolean {
-    let character = characters.find((character) => character.name === name)
-    if (!character) return false; // En caso de que character sea undefined
+export function updateCharacter(name: string, level?: number, health?: number, experience?: number){
+    try {
+        let character = characters.find((character) => character.name === name)
+        if (!character) return false; // En caso de que character sea undefined
+    
+        if (level !== undefined) character.level = level;
+        if (health !== undefined) character.health = health;
+        if (experience !== undefined) character.experience = experience;
 
-    if (level !== undefined) character.level = level;
-    if (health !== undefined) character.health = health;
-    if (experience !== undefined) character.experience = experience;
-
-    return true;
+    } catch (error) {
+        console.error("Error al actualizar el personaje", error)
+    }
 }
 
 export function deleteCharacter(name: string){
-    return characters = characters.filter((character) => character.name !== name)
+    try {
+        return characters = characters.filter((character) => character.name !== name)
+    } catch (error) {
+        console.error("Error al eliminar el personaje", error)
+    }
 }
